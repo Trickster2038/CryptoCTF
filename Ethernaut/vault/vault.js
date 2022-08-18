4331305784b1f9372718e2f76919eb9fd3469fb9
Astakhov Sergey <fzastahov@gmail.com>
2022-08-19 01:52:56 +0300

    ethernaut - vault

---
+++ Ethernaut/vault/vault.js
@@ -0,0 +1,4 @@
+await web3.eth.getStorageAt("0xb40fE780247a1c0D98abF792C215a4a53Ee7b0Fe", 1, console.log)
+
+// password got from storage
+contract.unlock("0x412076657279207374726f6e67207365637265742070617373776f7264203a29")
